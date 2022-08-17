ec6b631b6c8fdb4987f276bc5aafe2e706bc379b
Muhammed Enes <75534004+enes395@users.noreply.github.com>
2022-08-17 13:44:13 +0300

    Update SettingsViewController.swift

--- InstagramCloneFirebase/InstagramCloneFirebase/SettingsViewController.swift
+++ InstagramCloneFirebase/InstagramCloneFirebase/SettingsViewController.swift
@@ -7,25 +7,26 @@
 
 import UIKit
 import Firebase
+
 class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-     
+        // Do any additional setup after loading the view.
     }
     
-
- 
     @IBAction func logoutClicked(_ sender: Any) {
         do {
-                 try Auth.auth().signOut()
-                 self.performSegue(withIdentifier: "toViewController", sender: nil)
-                 
-             } catch {
-                 print("error")
-             }
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "toViewController", sender: nil)
+            
+        } catch {
+            print("error")
+        }
+        
     }
     
-    
+  
+
 }
